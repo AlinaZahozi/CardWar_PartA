c3fa36af1b092cf71c2a9e61ddd0c542234273fb
AlinaZ <93255163+AlinaZahozi@users.noreply.github.com>
2023-03-29 18:41:16 +0300

    Update

--- Test.cpp
+++ Test.cpp
@@ -100,7 +100,7 @@ TEST_CASE("Test 11-Test Game class method playAll()") {
     Game game(p1,p2); 
     game.playAll();
     CHECK(game.get_log() != "");
-    CHECK(game.get_log() == "");
+    CHECK((p1.get_total_wins() + p2.get_total_wins()) >= 1);
     
 }
 
@@ -110,4 +110,70 @@ TEST_CASE("Test 12-Test Game class method playAll() after finishing the game") {
     Game game(p1,p2); 
     game.playAll();
     CHECK_NOTHROW(game.playAll());
+}
+
+TEST_CASE("Test 13-Test Game class method printLastTurn()") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    game.playAll();
+    CHECK(game.get_log() != "");
+    CHECK_NOTHROW(game.printLastTurn());
+}
+
+TEST_CASE("Test 14-Test Game class method printLastTurn()") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    for (int i=0;i<7;i++) {
+     game.playTurn();
+    }
+    CHECK(game.get_log() != "");
+    CHECK_NOTHROW(game.printLastTurn());
+}
+
+TEST_CASE("Test 15-Test Game class method printWiner() befor game finish") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    CHECK_THROWS(game.printWiner());
+}
+
+TEST_CASE("Test 16-Test Game class method printWiner() after game finish") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    game.playAll();
+    CHECK(game.get_log() != "");
+    CHECK_NOTHROW(game.printWiner());
+}
+
+TEST_CASE("Test 17-Test Game class method printLog() befor any turn was performed") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    CHECK_NOTHROW(game.printLog());
+}
+
+TEST_CASE("Test 18-Test Game class method printLog() after game finish") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    game.playAll();
+    CHECK_NOTHROW(game.printLog());
+}
+
+TEST_CASE("Test 19-Test Game class method printStats() befor any turn was performed") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    CHECK_NOTHROW(game.printStats());
+}
+
+TEST_CASE("Test 20-Test Game class method printStats() after game finish") {
+    Player p1("Alina");
+    Player p2("Alin");
+    Game game(p1,p2); 
+    game.playAll();
+    CHECK_NOTHROW(game.printStats());
 }
